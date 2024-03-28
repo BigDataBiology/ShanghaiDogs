99f333899aec8856b1c477f952566e32f51686a9
a-89 <a.89@users.noreply.github.com>
2024-03-28 15:27:09 +0800

    ENH identify species that are commonly >99% identical in SH dog cohort

--- analysis/mag_sharing.py
+++ analysis/mag_sharing.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 import os
+import numpy as np
 
 os.chdir('/data/Projects/ShanghaiDogs/analysis')
 data = pd.read_csv('../intermediate-outputs/05_dereplication/01_drep/ANI_9999/data_tables/Ndb.csv')
+mimag_tab = pd.read_csv('../data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv',index_col=0)
 metadata = pd.read_csv('../data/ShanghaiDogsMetadata/SH_Dog_metadata_red.csv', index_col=0)
 household = metadata['Household.1'].to_dict()
 household['D000'] = household['D008']
@@ -34,6 +36,54 @@ sns.boxplot(data=paired_10, x='is_same_household', y='fraction_shared_st', ax=ax
 sns.stripplot(data=paired_10, x='is_same_household', y='fraction_shared_st', ax=ax)
 ax.text(0.5, 0.99, f'Mann-Whitney U test: p-value = {m_v.pvalue:.1e}', ha='center', va='center', transform=ax.transAxes)
 sns.despine(fig, trim=True)
-fig.savefig('figures/mag_sharing.pdf')
+fig.savefig('figures/mag_sharing_ANI.pdf')
 
 print(f'''Mann-Whitney U test: p-value = {m_v.pvalue}''')
+
+## Find bacterial species that are commonly >99% identical across SH dog cohort
+## Find taxonomy of highly-shared MAGs
+
+data['reference'] = data['reference'].str.replace('.fa.gz','')
+data['querry'] = data['querry'].str.replace('.fa.gz','')
+data_tax = pd.merge(data,mimag_tab[['Classification','Original ID']],left_on='reference',right_on='Original ID')
+data_tax = pd.merge(data_tax,mimag_tab[['Classification','Original ID']],left_on='querry',right_on='Original ID')
+data_tax.rename(columns={'Classification_x': 'Ref Classification', 'Classification_y': 'Qry Classification'}, inplace=True)
+data_tax.drop(['Original ID_x','Original ID_y'],axis=1,inplace=True)
+
+# Remove comparisons with eachself
+mask = data_tax['reference'] != data_tax['querry']
+data_tax = data_tax[mask]
+
+# Keep within species comparisons only (according to GTDB-tk)
+diffs = data_tax['Ref Classification'] != data_tax['Qry Classification']
+diffs = data_tax[diffs]
+print(diffs)
+
+same_sp = data_tax['Ref Classification'] == data_tax['Qry Classification']
+data_tax_sp = data_tax[same_sp]
+
+# Proportion of same strain within the same species
+data_tax_99 = data_tax_sp[data_tax_sp['ani']>0.99]
+tax_grouping_99 = data_tax_99['Ref Classification'].value_counts().reset_index()
+tax_grouping = data_tax['Ref Classification'].value_counts().reset_index()
+
+merged_tax = pd.merge(tax_grouping_99,tax_grouping,right_on='Ref Classification',left_on='Ref Classification')
+merged_tax = merged_tax[merged_tax['count_y'] > 300]
+merged_tax['comparisons >99 ANI'] = merged_tax['count_x']/merged_tax['count_y']*100
+merged_tax.columns = ['Classification','ani >=99%','ani >=95%','% ani >=99% if same sp']
+
+merged_tax[['Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']] = \
+    merged_tax['Classification'].str.split(';', expand=True)
+
+merged_tax_sorted = merged_tax.sort_values(by='% ani >=99% if same sp', ascending=False)
+high_st_sharing = merged_tax_sorted[merged_tax_sorted['% ani >=99% if same sp']>70]
+low_st_sharing = merged_tax_sorted[merged_tax_sorted['% ani >=99% if same sp']<10]
+
+plt.figure(figsize=(8, 4))
+order_colors = {phylum: plt.cm.tab20(i) for i, phylum in enumerate(merged_tax_sorted['Phylum'].unique())}
+plt.bar(high_st_sharing['Species'], high_st_sharing['% ani >=99% if same sp'], \
+        color=[order_colors[order] for order in high_st_sharing['Phylum']])
+plt.xticks(rotation=30, ha='right')
+plt.ylim(0,100)
+plt.tight_layout()
+plt.show()
