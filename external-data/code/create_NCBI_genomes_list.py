38d66197fb22e938ea2fad3918a33e99e9130a92
a-89 <a.89@users.noreply.github.com>
2024-04-11 15:13:23 +0800

    ADD create the list of NCBI assembly accession numbers used as fastani_ref in GTDBtk

---
+++ external-data/code/create_NCBI_genomes_list.py
@@ -0,0 +1,14 @@
+import os 
+import pandas as pd
+
+os.chdir('/data/Projects/ShanghaiDogs/')
+
+qual_report = pd.read_csv('data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv')
+NCBI_ref = qual_report['GTDBtk fastani Ref'].dropna().unique().tolist()
+print(NCBI_ref)
+
+out_path = "external-data/data/GTDBtk_fastani_ref_genomes/fastani_ref_genome_assemblies.txt"
+
+with open(out_path, 'w') as file:
+    for assembly in NCBI_ref:
+        file.write(str(assembly) + '\n')
