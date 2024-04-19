ac1844eb3588b082c6f6206883b1675bf0141e3e
a-89 <a.89@users.noreply.github.com>
2024-04-19 16:37:14 +0800

    RFCT Simpler code

--- analysis/eggNOG-mobilome-SHD_MAG-GTDBtk_Ref.py
+++ analysis/eggNOG-mobilome-SHD_MAG-GTDBtk_Ref.py
@@ -34,17 +34,16 @@ ext_path = 'external-data/data/NCBI_genomes_ref/eggNOG-annot/'
 ext_mobilome = []
 
 for filename in glob.glob(os.path.join(ext_path, '*.annotations')):
-  with open(filename, 'r') as file:
-      annot = pd.read_csv(filename,skiprows=range(4),sep='\t')
-      pattern = r'(GCA|GCF)_\d+\.\d+'
-      match = re.search(pattern, filename)
-      name = match.group(0)
-      annot['eggNOG_OGs'].fillna('', inplace=True)
-      mob_annot = annot[annot['eggNOG_OGs'].str.contains('|'.join(COG_X_ls))]
-      mob_counts = mob_annot['eggNOG_OGs'].value_counts().reset_index()
-      mob_counts.columns = ['eggNOG_OGs','Count']
-      mob_counts['Name'] = name
-      ext_mobilome.append(mob_counts)
+    annot = pd.read_csv(filename,skiprows=range(4),sep='\t')
+    pattern = r'(GCA|GCF)_\d+\.\d+'
+    match = re.search(pattern, filename)
+    name = match.group(0)
+    annot['eggNOG_OGs'].fillna('', inplace=True)
+    mob_annot = annot[annot['eggNOG_OGs'].str.contains('|'.join(COG_X_ls))]
+    mob_counts = mob_annot['eggNOG_OGs'].value_counts().reset_index()
+    mob_counts.columns = ['eggNOG_OGs','Count']
+    mob_counts['Name'] = name
+    ext_mobilome.append(mob_counts)
 
 ext_mobilome_df = pd.concat(ext_mobilome, ignore_index=True)
 
@@ -52,17 +51,16 @@ shd_path = 'intermediate-outputs/eggNOG-annot/'
 shd_mobilome = []
 
 for filename in glob.glob(os.path.join(shd_path, '*.annotations')):
-  with open(filename, 'r') as file:
-      annot = pd.read_csv(filename,skiprows=range(4),sep='\t')
-      pattern = r'SHD\d+_\d+'
-      match = re.search(pattern, filename)
-      name = match.group(0)
-      annot['eggNOG_OGs'].fillna('', inplace=True)
-      mob_annot = annot[annot['eggNOG_OGs'].str.contains('|'.join(COG_X_ls))]
-      mob_counts = mob_annot['eggNOG_OGs'].value_counts().reset_index()
-      mob_counts.columns = ['eggNOG_OGs','Count']
-      mob_counts['Name'] = name
-      shd_mobilome.append(mob_counts)
+    annot = pd.read_csv(filename,skiprows=range(4),sep='\t')
+    pattern = r'SHD\d+_\d+'
+    match = re.search(pattern, filename)
+    name = match.group(0)
+    annot['eggNOG_OGs'].fillna('', inplace=True)
+    mob_annot = annot[annot['eggNOG_OGs'].str.contains('|'.join(COG_X_ls))]
+    mob_counts = mob_annot['eggNOG_OGs'].value_counts().reset_index()
+    mob_counts.columns = ['eggNOG_OGs','Count']
+    mob_counts['Name'] = name
+    shd_mobilome.append(mob_counts)
 
 shd_mobilome_df = pd.concat(shd_mobilome, ignore_index=True)
 
@@ -70,7 +68,7 @@ a = pd.merge(shd_mobilome_df,SHD_Ref,left_on='Name',right_on='Bin ID')
 a.drop('Bin ID',axis=1,inplace=True)
 
 all_COGs=pd.merge(a,ext_mobilome_df,left_on=['GTDBtk fastani Ref','eggNOG_OGs'],right_on=['Name','eggNOG_OGs'],how='outer')
-all_COGs.fillna('0', inplace=True)
+all_COGs.fillna(0, inplace=True)
 all_COGs['Count_x']=(all_COGs['Count_x']).astype(float)
 all_COGs['Count_y']=(all_COGs['Count_y']).astype(float)
 
@@ -107,8 +105,10 @@ plt.show()
 sub_mob_counts_COG = sub_mob_counts_COG.set_index('COG_id')
 
 # Create the heatmap
-plt.figure(figsize=(7,9))
-sns.heatmap(sub_mob_counts_COG[['SHD Counts', 'GTDBtk Ref Counts']], cmap='Blues', annot=True, fmt="g", linewidths=0.5,linecolor='black')
+fig,ax = plt.subplots(figsize=(7,9))
+sns.heatmap(
+        sub_mob_counts_COG[['SHD Counts', 'GTDBtk Ref Counts']],
+            cmap='Blues', annot=True, fmt="g", linewidths=0.5,linecolor='black')
 plt.xlabel('Count')
 plt.ylabel('Name')
 plt.tight_layout()
