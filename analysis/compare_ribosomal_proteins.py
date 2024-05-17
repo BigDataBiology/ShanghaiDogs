71fc7eb1d46194a324d658fe7a1d0949fc653ebf
a-89 <a.89@users.noreply.github.com>
2024-05-17 10:09:54 +0800

    ENH Exclude partial 16S rRNA genes

--- analysis/compare_ribosomal_proteins.py
+++ analysis/compare_ribosomal_proteins.py
@@ -1,4 +1,5 @@
 from jug import TaskGenerator
+from jug import mapreduce
 from jug.utils import cached_glob
 
 RIBOSOMAL_DIR = '../intermediate-outputs/07_ribosomal_genes/barrnap_fasta/'
@@ -22,7 +23,7 @@ def compare_seqs(seq1, seq2):
     return same/n
 
 @TaskGenerator
-def compare_all_pairwise(ifile, name_filter):
+def compare_all_pairwise(ifile, name_filter, min_length=None):
     '''Compare all 16s sequences in a file pairwise
 
     Returns a matrix of identities
@@ -33,6 +34,7 @@ def compare_all_pairwise(ifile, name_filter):
     selected = []
     for name, seq in fasta_iter(ifile):
         if name.startswith(name_filter):
+            if min_length is not None and len(seq) < min_length: continue
             selected.append(seq)
     selected = [skbio.sequence.DNA(seq) for seq in selected]
 
@@ -44,8 +46,36 @@ def compare_all_pairwise(ifile, name_filter):
 
     return identity
 
-for ifile in cached_glob(f'{RIBOSOMAL_DIR}/*/*_ribosomal.fa'):
-    compare_all_pairwise(ifile, '16S_rRNA')
-    compare_all_pairwise(ifile, '23S_rRNA')
-    compare_all_pairwise(ifile,  '5S_rRNA')
+def min_nz_value(vals):
+    from scipy import spatial
+    import numpy as np
+    if len(vals) <= 1:
+        return np.nan
+    return spatial.distance.squareform(vals).min()
 
+@TaskGenerator
+def build_results_table(fs, mv_16, mv_23, mv_5):
+    import pandas as pd
+    mags = [f.split('/')[-1] for f in fs]
+    return pd.DataFrame({
+        'min_id_16s': mv_16,
+        'min_id_23s': mv_23,
+        'min_id_5s': mv_5,
+        }, index=mags)
+
+ribosomal_files = cached_glob(f'{RIBOSOMAL_DIR}/*/*_ribosomal.fa')
+ids_16s = []
+ids_16s_full = []
+ids_23s = []
+ids_5s  = []
+for ifile in ribosomal_files:
+    ids_16s.append(compare_all_pairwise(ifile, '16S_rRNA', min_length=1200))
+    ids_16s_full.append(compare_all_pairwise(ifile, '16S_rRNA'))
+    ids_23s.append(compare_all_pairwise(ifile, '23S_rRNA'))
+    ids_5s .append(compare_all_pairwise(ifile,  '5S_rRNA'))
+
+min_vals_16s = mapreduce.map(min_nz_value, ids_16s, map_step=64)
+min_vals_23s = mapreduce.map(min_nz_value, ids_23s, map_step=64)
+min_vals_5s  = mapreduce.map(min_nz_value,  ids_5s, map_step=64)
+
+final = build_results_table(ribosomal_files, min_vals_16s, min_vals_23s, min_vals_5s)
