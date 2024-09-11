a8784d2506dcf21ffca186a4c60b33c9c2b8bf9b
Luis Pedro Coelho <luis@luispedro.org>
2024-09-11 12:44:40 +1000

    ENH Map to species representatives

--- resource_generation/Coelho_2018_mappings/create-reference.py
+++ resource_generation/Coelho_2018_mappings/create-reference.py
@@ -1,27 +1,40 @@
+import pandas as pd
 import subprocess
 import gzip
 import os
 from glob import glob
+from jug import TaskGenerator
 
 basedir = '../../data/ShanghaiDogsMAGs/'
 outdir = '../../intermediate-outputs/Coelho_2018_mappings/'
-ofile = outdir + 'ShanghaiDogsMAGs.fna.gz'
+shd_meta = pd.read_csv('../../data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv', index_col=0)
 
-if os.path.exists(ofile):
-    print(f'{ofile} already exists. Exiting.')
-    exit()
+@TaskGenerator
+def create_reference(ofile, only_reps=False):
+    if os.path.exists(ofile):
+        print(f'{ofile} already exists. Exiting.')
+        return ofile
 
-os.makedirs(outdir, exist_ok=True)
-with open(ofile + '.tmp', 'wb') as tmp_out:
-    gz_p = subprocess.Popen(['gzip'], stdin=subprocess.PIPE, stdout=tmp_out)
-    for file in sorted(glob(basedir + '*.fna.gz')):
-        print(f'Processing {file}')
-        with gzip.open(file, 'rb') as f:
-            while ch := f.read(8192):
-                gz_p.stdin.write(ch)
-    gz_p.stdin.close()
-    if gz_p.wait() != 0:
-        raise OSError(f'Error creating {ofile}')
+    os.makedirs(outdir, exist_ok=True)
 
-os.rename(ofile + '.tmp', ofile)
+    with open(ofile + '.tmp', 'wb') as tmp_out:
+        gz_p = subprocess.Popen(['gzip'], stdin=subprocess.PIPE, stdout=tmp_out)
+        for file in sorted(glob(basedir + '*.fna.gz')):
+            print(f'Processing {file}')
+            bin_name = os.path.basename(file)
+            if only_reps and shd_meta.loc[bin_name, 'Representative'] != 'Yes':
+                print(f'Skipping {bin_name} as it is not a representative.')
+                continue
+            with gzip.open(file, 'rb') as f:
+                while ch := f.read(8192):
+                    gz_p.stdin.write(ch)
+        gz_p.stdin.close()
+        if gz_p.wait() != 0:
+            raise OSError(f'Error creating {ofile}')
+
+    os.rename(ofile + '.tmp', ofile)
+    return ofile
+
+create_reference(outdir + 'ShanghaiDogsMAGs.fna.gz', only_reps=False)
+create_reference(outdir + 'ShanghaiDogsMAGsSpecies.fna.gz', only_reps=True)
 
