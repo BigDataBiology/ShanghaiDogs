fc6470c045d8c52c8d55d4ba084befa73cf7e003
Luis Pedro Coelho <luis@luispedro.org>
2024-09-13 15:10:53 +1000

    Berlin + Yarlagadda mappings (#2)
    * ENH Map to species representatives
    
    * ENH Map Berlin dog data
    
    * ENH Map Yarlagadda dogs as well
    
    Generalize the previous map NGLess script to take a YAML file as argument
    
    * ENH Aggregate all mapping rate data
    
    * ENH Plot mapping rates for external datasets

--- resource_generation/Coelho_2018_mappings/aggregate-results.py
+++ resource_generation/Coelho_2018_mappings/aggregate-results.py
@@ -1,21 +1,45 @@
-import numpy as np
 import pandas as pd
 from glob import glob
-from matplotlib import pyplot as plt
-import seaborn as sns
+import yaml
 
 shd_mapstats = glob('outputs/*_shd_mapstats.txt')
+shd_sp_mapstats = glob('outputs/*_shd_sp_mapstats.txt')
 
-fraction = {}
+aligned = {}
 for file in sorted(shd_mapstats):
     sample = file.split('_')[0].split('/')[-1]
-    fraction[sample] = pd.read_csv(file, sep='\t', index_col=0).T.eval('aligned/total').item()
+    aligned[sample] = pd.read_csv(file, sep='\t', index_col=0).squeeze()[['total', 'aligned']]
+aligned = pd.DataFrame.from_dict(aligned, orient='index')
+
+aligned_sp = {}
+for file in sorted(shd_sp_mapstats):
+    sample = file.split('_')[0].split('/')[-1]
+    aligned_sp[sample] = pd.read_csv(file, sep='\t', index_col=0).squeeze()[['total', 'aligned']]
+aligned_sp = pd.DataFrame.from_dict(aligned_sp, orient='index')
 
 
-fraction = pd.Series(fraction)
-fraction = fraction.sort_values()
-pd.DataFrame({'fraction': fraction}) \
+data = pd.DataFrame(
+               {'total': aligned['total']
+                ,'aligned': aligned['aligned']
+                ,'total_sp': aligned_sp['total']
+                ,'aligned_sp': aligned_sp['aligned']
+              }) \
     .reset_index() \
-    .rename(columns={'index': 'sample'}) \
-    .to_csv('outputs/fraction_shd_mapped.tsv', sep='\t', index=False)
+    .rename(columns={'index': 'sample'})
 
+assert data.dropna().eval('total == total_sp').all()
+
+data.drop('total_sp', axis=1, inplace=True)
+
+nestle = set(line.strip() for line in open('../../external-data/data/Coelho_dogs_2018/PRJEB20308'))
+berlin = set(s.split('_')[0] for s in yaml.safe_load(open('berlin_dogs.yaml'))['samples'])
+Yarlagadda = set(yaml.safe_load(open('Yarlagadda_dogs.yaml'))['samples'])
+def group_for(s):
+    if s in nestle:
+        return 'Nestlé'
+    if s in berlin:
+        return 'Berlin'
+    if s in Yarlagadda:
+        return 'Yarlagadda'
+data['group'] = data['sample'].map(group_for)
+data.to_csv('../../intermediate-outputs/external_datasets_mappings/reads_mapped_shd.tsv', sep='\t', index=False)
