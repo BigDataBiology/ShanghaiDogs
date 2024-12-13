68075eb1285a2632ac641b3f134757432ada6899
a-89 <a.89@users.noreply.github.com>
2024-12-13 18:31:03 +0800

    ENH pre-process, merge and filter RGI contig-level output

---
+++ analysis/RGI_output_preprocess.py
@@ -0,0 +1,65 @@
+import sys
+import pandas as pd
+import os
+
+os.chdir('/data/Projects/ShanghaiDogs')
+
+# Import files
+
+# MIMAG report
+MIMAG_report = pd.read_csv("data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv", \
+                  delimiter=',',header=0)
+
+# concatenated RGI results on MAGs
+arg_MAGs = pd.read_csv("intermediate-outputs/06_ARG/00_RGI_CARD/ALL_samples_strict_matched.txt", \
+                       delimiter='\t',header=None,index_col=None)
+
+# concatenated RGI results on contigs
+arg_contigs = pd.read_csv("intermediate-outputs/06_ARG/01_RGI_CARD_contigs/contigs-ARGs_ALL.txt", \
+                          delimiter='\t',header=None,index_col=None)
+
+
+# Reformat RGI output tables
+
+## 1) For arg_MAGs
+arg_MAGs.columns = ["ORF_ID", "Contig", "Start", "Stop", "Orientation", "Cut_Off", "Pass_Bitscore", "Best_Hit_Bitscore",
+                    "Best_Hit_ARO", "Best_Identities", "ARO", "Model_type", "SNPs_in_Best_Hit_ARO", "Other_SNPs",
+                    "Drug Class", "Resistance Mechanism", "AMR Gene Family", "Predicted_DNA", "Predicted_Protein",
+                    "CARD_Protein_Sequence", "Percentage Length of Reference Sequence", "ID", "Model_ID", "Nudged", "Note",
+                    "Hit_Start", "Hit_End", "Antibiotic"]
+
+arg_MAGs['sample_id'] = arg_MAGs['ORF_ID'].str.extract(r'(\bD[0-9]+\b)')
+arg_MAGs['bin_id'] = arg_MAGs['ORF_ID'].str.extract(r'(out_[^\/]+_SemiBin_[^\.]+)')
+arg_MAGs['bin_id'] = arg_MAGs['bin_id'].str.replace('out_','')
+arg_MAGs['bin_id'] = arg_MAGs['bin_id'] + '_' + arg_MAGs['sample_id']
+arg_MAGs['partial']=arg_MAGs['ORF_ID'].str.extract(r'partial=(\d{2})')
+
+arg_MAGs_rf=pd.merge(arg_MAGs,MIMAG_report[['Original ID','Quality','Bin ID']],left_on='bin_id',right_on='Original ID')
+
+# Stricter filtering
+arg_MAGs_filt = arg_MAGs_rf[arg_MAGs_rf['Best_Identities']>=90]
+arg_MAGs_filt = arg_MAGs_filt[arg_MAGs_filt['Percentage Length of Reference Sequence']>=90]
+
+arg_MAGs_filt.to_csv('intermediate-outputs/06_ARG/MAGs-ARGs_ALL_filt.txt')
+
+## 2) For arg_contigs
+arg_contigs.columns = ["ORF_ID", "Contig", "Start", "Stop", "Orientation", "Cut_Off", "Pass_Bitscore", "Best_Hit_Bitscore",
+                    "Best_Hit_ARO", "Best_Identities", "ARO", "Model_type", "SNPs_in_Best_Hit_ARO", "Other_SNPs",
+                    "Drug Class", "Resistance Mechanism", "AMR Gene Family", "Predicted_DNA", "Predicted_Protein",
+                    "CARD_Protein_Sequence", "Percentage Length of Reference Sequence", "ID", "Model_ID", "Nudged", "Note",
+                    "Hit_Start", "Hit_End", "Antibiotic", "sample_id"]
+
+arg_contigs['partial']=arg_contigs['ORF_ID'].str.extract(r'partial=(\d{2})')
+
+# Format is weird, so I extract the information from the ORF_id column
+arg_contigs['Contig'] = arg_contigs['ORF_ID'].str.extract(r'(contig_[^ ]*\d+)')
+arg_contigs['Start'] = arg_contigs['ORF_ID'].str.extract(r'#\s*(\d+)')
+arg_contigs['Stop'] = arg_contigs['ORF_ID'].str.extract(r'#\s*\d+\s*#\s*(\d+)')
+arg_contigs['Orientation'] = arg_contigs['ORF_ID'].str.extract(r'#\s*\d+\s*#\s*\d+\s*#\s*([+-])')
+arg_contigs['Orientation'] = arg_contigs['Orientation'].fillna('+')
+
+# Stricter filtering
+arg_contigs_filt = arg_contigs[arg_contigs['Best_Identities']>=90]
+arg_contigs_filt = arg_contigs_filt[arg_contigs_filt['Percentage Length of Reference Sequence']>=90]
+
+arg_contigs_filt.to_csv('intermediate-outputs/06_ARG/contigs-ARGs_ALL_filt.txt')
--- resource_generation/run_rgi_contigs.sh
+++ resource_generation/run_rgi_contigs.sh
@@ -14,3 +14,12 @@ for n in {00..52}
     rgi main --input_sequence ${prodigal_contigs}/D0${n}/D0${n}_proteins.faa \
     --output_file ${output_folder}/D0${n}/out_D0${n} -t protein --clean
   done
+
+for n in {00..52}
+  do
+    grep -h -e "Strict" -e "Perfect" ${output_folder}/D0${n}/*.txt | \
+    awk -v sample_id="D0${n}" '{sub(/\r$/, ""); print $0 "\t" sample_id}' > \
+    ${output_folder}/D0${n}/D0${n}_ARGs_formatted.txt
+  done
+
+cat ${output_folder}/D0*/*ARGs_formatted.txt > ${output_folder}/contigs-ARGs_ALL_samples.txt
