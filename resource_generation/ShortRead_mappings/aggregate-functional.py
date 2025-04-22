494a163872f6d0b6d58291cbbb8daddb564d9f13
Luis Pedro Coelho <luis@luispedro.org>
2025-04-22 12:57:25 +1000

    ENH More functional profiles
    - KEGG modules
    - CAZy
    - COG categories

--- resource_generation/ShortRead_mappings/aggregate-functional.py
+++ resource_generation/ShortRead_mappings/aggregate-functional.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from glob import glob
 
-kegg_kos = sorted(glob('outputs/functional/*KEGG_ko.tsv.gz'))
-data = pd.concat([pd.read_csv(f, sep='\t', index_col=0).squeeze() for f in kegg_kos], axis=1)
-data.index.name = 'KO'
-data.to_csv('outputs/KEGG_ko.tsv.gz', sep='\t', index=True, header=True)
+for v in ['KEGG_ko', 'CAZy', 'KEGG_Module', 'COG_category']:
+    partials = glob(f'outputs/functional/*{v}.tsv.gz')
+    partials.sort()
+    data = pd.concat([pd.read_csv(f, sep='\t', index_col=0).squeeze() for f in partials], axis=1)
+    data.index.name = v
+    data.to_csv(f'outputs/functional_tables/{v}.tsv.gz', sep='\t', index=True, header=True)
+    print(f'Saved {v} ...', data.shape)
