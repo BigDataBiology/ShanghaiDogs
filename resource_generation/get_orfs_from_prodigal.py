2f0b365e93fd392f07df86ce513021561d849ed9
Luis Pedro Coelho <luis@luispedro.org>
2025-06-16 14:20:27 +1000

    ENH Better unigenes statistics
    Quantify fraction of partial unigenes & unigenes in ECEs

--- resource_generation/get_orfs_from_prodigal.py
+++ resource_generation/get_orfs_from_prodigal.py
@@ -7,13 +7,11 @@ from jug import TaskGenerator
 
 BASE_FNA = '../data/ShanghaiDogsAssemblies'
 BASE_PRODIGAL = '../intermediate-outputs/Prodigal'
-PAT = r'>(\S+)_(\d+) # (\d+) # (\d+) # (-?\d+)'
+PAT = r'>(\S+)_(\d+) # (\d+) # (\d+) # (-?\d+) # ID=\d+_\d+;partial=(\d+)'
+
 def rc(s):
     return s.translate(str.maketrans('ACGT', 'TGCA'))[::-1]
 
-def pad9(ix):
-    ix = f'{ix:09}'
-    return f'{ix[:3]}_{ix[3:6]}_{ix[6:]}'
 
 @TaskGenerator
 def recover_fna_from_prodigal_faa(sample):
@@ -23,33 +21,37 @@ def recover_fna_from_prodigal_faa(sample):
     for line in gzip.open(faafile, 'rt'):
         if line.startswith('>'):
             if m := re.match(PAT, line):
-                contig, gene_id, start, end, strand = m.groups()
-                orfs[contig].append((f'{contig}_{gene_id}', int(start), int(end), int(strand)))
+                contig, gene_id, start, end, strand, partial = m.groups()
+                orfs[contig].append((f'{contig}_{gene_id}', int(start), int(end), int(strand), partial))
+            else:
+                raise ValueError(f'Unexpected header format: {line.strip()}')
 
     contigs = f'{BASE_FNA}/{sample}_PP1_PolcaCorr.fna.gz'
     with gzip.open(fnafile, 'wt') as f_out:
         for h, s in fasta.fasta_iter(contigs):
-            for orf_id, start, end, strand in orfs.get(h, []):
+            for orf_id, start, end, strand, partial in orfs.get(h, []):
                 orf_seq = s[start-1:end]
                 if strand == -1:
                     orf_seq = rc(orf_seq)
-                f_out.write(f'>{orf_id}\n{orf_seq}\n')
+                f_out.write(f'>{orf_id} {start} {end} {strand} {partial}\n{orf_seq}\n')
     return fnafile
+
 
 @TaskGenerator
 def collate_orfs(fnafiles):
-    import pandas as pd
     import lib
     ofile = f'{BASE_PRODIGAL}/SHD.ORF.fna.xz'
     ix = 0
     with lib.xz_out(ofile) as out:
         with lib.xz_out(f'{BASE_PRODIGAL}/SHD.ORF.orig.tsv.xz') as o_out:
+            o_out.write(f'ORF\tSample\tOriginal_ID\tStart\tEnd\tStrand\tPartial\n'.encode('ascii'))
             for f in fnafiles:
-                for h,seq in fasta.fasta_iter(f):
-                    orf_id = 'SHD.ORF.'+pad9(ix)
+                for h,seq in fasta.fasta_iter(f, full_header=True):
+                    orf_id = lib.pad9('SHD.ORF', ix)
                     sample = f.split('/')[-1].split('_')[0]
                     ix += 1
-                    o_out.write(f'{orf_id}\t{sample}\t{h}\n'.encode('ascii'))
+                    orig_h, start, end, strand, partial = h.split()
+                    o_out.write(f'{orf_id}\t{sample}\t{orig_h}\t{start}\t{end}\t{strand}\t{partial}\n'.encode('ascii'))
                     out.write(f'>{orf_id}\n{seq}\n'.encode('ascii'))
     return ofile
 
