d4699cd0a0afe5f511d47442be0aa8d9f1c35a68
Kruthi Rao <82725469+Kruthimacha@users.noreply.github.com>
2025-06-23 09:10:06 +1000

    Update Potential_pathogens.py with calculated median for potential pathogens

--- analysis/Potential_pathogens.py
+++ analysis/Potential_pathogens.py
@@ -1,18 +1,30 @@
 import pandas as pd
+import os
 
-# paths
-mimag_report_path = 'shanghai_dogs/data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv'
-args_filt_path = 'microbiome/shanghai_dogs/intermediate-outputs/06_ARG/MAGs-ARGs_ALL_filt.txt'
+#paths
+mimag_report_path = 'data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv'
+args_filt_path = 'intermediate-outputs/06_ARG/MAGs-ARGs_ALL_filt.txt'
+
 mimag_df = pd.read_csv(mimag_report_path)
 args_df = pd.read_csv(args_filt_path)
 
-# extract species
+# Extract species
 mimag_df['Species'] = mimag_df['Classification'].str.extract(r's__([^;]+)')
-
-# table with all Bin IDs and species
 base_df = mimag_df[['Bin ID', 'Species']].copy()
 
-# count ARGs per Bin
+# potential pathogens to filter
+pathogens = [
+    'Campylobacter_D upsaliensis', 'Enterobacter hormaechei_A', 'Enterobacter roggenkampii',
+    'Enterococcus faecalis', 'Enterococcus_B faecium', 'Enterococcus_B hirae',
+    'Enterococcus_B lactis', 'Enterococcus_D gallinarum', 'Helicobacter_A bilis_A',
+    'Helicobacter_A rappini', 'Helicobacter_B canis', 'Helicobacter_B canis_B',
+    'Helicobacter_C magdeburgensis', 'Klebsiella pneumoniae', 'Proteus mirabilis'
+]
+
+#base_df to include only potential pathogens
+base_df = base_df[base_df['Species'].isin(pathogens)]
+
+# Count ARGs per Bin
 arg_counts = args_df.pivot_table(
     index='Bin ID',
     columns='Best_Hit_ARO',
@@ -20,20 +32,18 @@ arg_counts = args_df.pivot_table(
     fill_value=0
 ).reset_index()
 
-# merge with ARG counts 
 full_df = pd.merge(base_df, arg_counts, on='Bin ID', how='left')
 
-# fbins with no ARGs with 0
+# Fill bins with no ARGs with 0
 full_df = full_df.fillna(0)
 
-#  columns to int
 for col in full_df.columns[2:]:
     full_df[col] = full_df[col].astype(int)
 
-# total ARGs column
-full_df['Total_ARGs'] = full_df.iloc[:, 2:].sum(axis=1)
+# Collapse by species, calculating median for each ARG
+collapsed_df = full_df.groupby('Species').median(numeric_only=True).reset_index()
 
-# save
-full_df.to_csv('ARGs_by_Bin_Summary.csv', index=False)
+# Add total ARGs column - sum of medians across ARGs for each species
+collapsed_df['Total_ARGs'] = collapsed_df.iloc[:, 1:].sum(axis=1)
 
-print("File saved as 'ARGs_by_Bin_Summary.csv'")
+print(collapsed_df)
