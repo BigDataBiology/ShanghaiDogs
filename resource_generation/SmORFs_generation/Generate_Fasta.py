7939e02b114f6760c54b34a98ac9b398a680ee00
Kruthi Rao <82725469+Kruthimacha@users.noreply.github.com>
2025-06-23 16:18:41 +1000

    Updated Generate_Fasta.py with symlinks

--- resource_generation/SmORFs_generation/Generate_Fasta.py
+++ resource_generation/SmORFs_generation/Generate_Fasta.py
@@ -1,44 +1,13 @@
 import os
 from collections import defaultdict
+import fasta  
+import lib    
 
-# paths
+# Paths
 base_dir = "/work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER"
 output_fasta = os.path.join(base_dir, "Mapped_SmORFs_sequences.faa")
 sequence_dict = defaultdict(list)
 id_counter = 0
-
-# parse FASTA file (I will add the symlink soon had issue with auhtnetication)
-def fasta_iter(fname, full_header=False):
-    header = None
-    chunks = []
-    if fname.endswith('.gz'):
-        import gzip
-        op = gzip.open
-    elif fname.endswith('.bz2'):
-        import bz2
-        op = bz2.open
-    elif fname.endswith('.xz'):
-        import lzma
-        op = lzma.open
-    else:
-        op = open
-    with op(fname, 'rt') as f:
-        for line in f:
-            if line[0] == '>':
-                if header is not None:
-                    yield header, ''.join(chunks)
-                line = line[1:].strip()
-                if not line:
-                    header = ''
-                elif full_header:
-                    header = line.strip()
-                else:
-                    header = line.split()[0]
-                chunks = []
-            else:
-                chunks.append(line.strip())
-        if header is not None:
-            yield header, ''.join(chunks)
 
 # Process each sample folder
 for sample in os.listdir(base_dir):
@@ -46,13 +15,13 @@ for sample in os.listdir(base_dir):
     if os.path.isdir(sample_path):
         fasta_file = os.path.join(sample_path, "mapped.smorfs.faa")
         if os.path.exists(fasta_file):
-            for header, seq in fasta_iter(fasta_file, full_header=True):
+            for header, seq in fasta.fasta_iter(fasta_file, full_header=True):
                 sequence_dict[seq].append(header)
 
-# unique sequences to output FASTA
+# Write unique sequences to output FASTA
 with open(output_fasta, "w") as out_f:
     for seq, headers in sequence_dict.items():
-        new_id = f"SHD.SM.100AA.000_{id_counter:03d}"
+        new_id = lib.pad9("SHD.SM.100AA", id_counter)  
         out_f.write(f">{new_id}\n{seq}\n")
         id_counter += 1
 
