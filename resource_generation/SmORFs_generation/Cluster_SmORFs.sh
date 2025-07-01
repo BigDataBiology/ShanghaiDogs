a42ebe6b0502f1868ae3812f3fbbc4ea7c859444
Kruthi Rao <82725469+Kruthimacha@users.noreply.github.com>
2025-07-01 10:25:26 +1000

    Update Cluster_SmORFs.sh with better comments

--- resource_generation/SmORFs_generation/Cluster_SmORFs.sh
+++ resource_generation/SmORFs_generation/Cluster_SmORFs.sh
@@ -1,3 +1,3 @@
-
-### Generate 90AA smORFs based on the 100AA smORFs uses CD - HIT
-./cd-hit -i /work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER/Mapped_SmORFs_sequences.faa -o /work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER/clustered_SmORFs -c 0.9 -n 5 -M 16000 -T 16 -d 0
+# Cluster 100AA smORFs at 90% sequence identity using CD-HIT to generate 90AA smORFs
+# Download cdhit: https://github.com/weizhongli/cdhit
+./cd-hit -i /work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER/100AA_SmORFs_sequences.faa -o /work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER/clustered_SmORFs -c 0.9 -n 5 -M 16000 -T 16 -d 0
