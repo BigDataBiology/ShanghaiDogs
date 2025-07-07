64f7b9f6972d4a7e0ae5bec79c4d2bcd80365b41
Luis Pedro Coelho <luis@luispedro.org>
2025-07-07 15:27:11 +1000

    ENH Consolidate 90AA resource generation

--- resource_generation/SmORFs_generation/SHD_Clusters.py
+++ resource_generation/SmORFs_generation/SHD_Clusters.py
@@ -1,13 +1,16 @@
 ## Map 100AA SmORFs to 90AA cluster representatives using CD-HIT results and export mappings to TSV
 import os
-import csv
+import gzip
 import re
 import fasta
 from collections import defaultdict
+import lib
+import pandas as pd
 
 #paths
-base_dir = "/work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER"
-input_fasta = os.path.join(base_dir, "100AA_SmORFs_sequences.faa")
+base_dir = "/work/microbiome/shanghai_dogs/intermediate-outputs/GMSC_MAPPER/"
+input_fasta = os.path.join(base_dir, "100AA_SmORFs_sequences.faa.gz")
+origin_tsv = os.path.join(base_dir, "100AA_SmORFs_origins.tsv.gz")
 cluster_file = os.path.join(base_dir, "clustered_SmORFs.clstr")
 output_tsv = os.path.join(base_dir, "SHD_Clusters.tsv")
 
@@ -47,25 +50,46 @@ missing_ids = cluster_ids - fasta_ids
 if missing_ids:
     raise ValueError(f"Sequence IDs in .clstr not found in FASTA: {missing_ids}")
 
+origins = pd.read_csv(origin_tsv, sep="\t")
+count100aa = origins['SmORF ID'].value_counts().to_dict()
+
+def count_smorfs(mapping_item):
+    # Sort by the number of SmORFs in the cluster and then by the minimum 100AA
+    # SmORF ID as a tiebreaker
+    return (
+            sum(
+                count100aa[seq_id] for seq_id, _ in mapping_item[1]
+                ),
+            min(seq_id for seq_id, _ in mapping_item[1])
+            )
+
+clusters = list(cluster_mappings.items())
+clusters.sort(key=count_smorfs, reverse=True)
 # Map 100AA to 90AA IDs
-id_mapping = {}
-for cluster, seqs in cluster_mappings.items():
-    representative_id = None
+mappings = []
+for idx, (_, seqs) in enumerate(clusters):
+    rep_id = lib.pad6("SHD1_SM.90AA", idx)
     for seq_id, similarity in seqs:
-        if similarity == "*":
-            representative_id = seq_id
-            break
-    if representative_id:
-        rep_90aa_id = representative_id.replace("100AA", "90AA")
-        for seq_id, similarity in seqs:
-            id_mapping[seq_id] = (rep_90aa_id, similarity)
+        mappings.append((seq_id, rep_id, similarity))
 
-# Write output TSV
-with open(output_tsv, "w", newline="") as out_f:
-    writer = csv.writer(out_f, delimiter="\t")
-    writer.writerow(["100AA SmORF ID", "90AA SmORF ID", "Similarity"])
-    for seq_id_100aa in sorted(id_mapping.keys()):
-        seq_id_90aa, similarity = id_mapping[seq_id_100aa]
-        writer.writerow([seq_id_100aa, seq_id_90aa, similarity])
+mappings = pd.DataFrame(mappings, columns=["100AA SmORF ID", "90AA SmORF ID", "Similarity"])
+mappings.sort_values(by=["100AA SmORF ID"], inplace=True)
+mappings.to_csv(output_tsv, sep="\t", index=False)
 
-print(f"Output TSV created: {output_tsv}")
+assert mappings.eval('Similarity == "*"').sum() == len(set(mappings['90AA SmORF ID']))
+mappings = mappings.query('Similarity == "*"')
+assert len(set(mappings['90AA SmORF ID'])) == len(set(mappings['100AA SmORF ID']))
+map_90aa_100aa = mappings.set_index('100AA SmORF ID')['90AA SmORF ID'].to_dict()
+
+seqs = []
+for h,seq in fasta.fasta_iter(input_fasta):
+    if h in map_90aa_100aa:
+        seqs.append((map_90aa_100aa[h], h, seq))
+assert len(seqs) == len(map_90aa_100aa)
+
+seqs.sort()
+
+with open(f"{base_dir}/SHD_90AA_SmORFs.faa", "wt") as out_f:
+    for h90, h100, seq in seqs:
+        out_f.write(f">{h90} {h100}\n{seq}\n")
+
