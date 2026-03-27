f5a1d76d076b0529de216ef27768b3f88b1d20cb
Luis Pedro Coelho <luis@luispedro.org>
2026-03-27 13:27:16 +1000

    ENH Rename contigs in barrnap outputs

--- resource_generation/organise-barrnap.py
+++ resource_generation/organise-barrnap.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from os import walk, makedirs
 import gzip
+import fasta
+from pathlib import Path
 meta = pd.read_csv('../data/ShanghaiDogsTables/SHD_bins_MIMAG_report.csv')
 meta.set_index('Original ID', inplace=True)
 
 TARGET_DIR = '../data/ShanghaiDogsMAGAnnotations/Barrnap/'
+FNA_DIR = Path('../data/ShanghaiDogsMAGs/')
 makedirs(TARGET_DIR, exist_ok=True)
 
 found = set()
@@ -21,9 +24,17 @@ for base,_,fs in walk('../intermediate-outputs/07_ribosomal_genes/barrnap_fasta/
                 raise SystemExit(1)
             found.add(fid)
             target_fname = meta.loc[fid, "Bin ID"].replace(".fna.gz", "_ribosomal.fna.gz")
+            fna_name = FNA_DIR / f"{meta.loc[fid, 'Bin ID']}"
+            renames = {}
+            for h,_ in fasta.fasta_iter(str(fna_name), full_header=True):
+                new,old,_ = h.split(' ', 2)
+                renames[old] = new
+
             with gzip.open(f'{TARGET_DIR}/{target_fname}', 'wb') as out_f:
-                with open(f'{base}/{f}_ribosomal.fa', 'rb') as in_f:
-                    while chunk := in_f.read(32 * 1024):
-                        out_f.write(chunk)
+                for h, seq in fasta.fasta_iter(f'{base}/{f}_ribosomal.fa', full_header=True):
+                    contig = h.split(':')[2]
+                    new_h = h.replace(contig, renames[contig])
+                    out_f.write(f'>{new_h}\n'.encode())
+                    out_f.write(f'{seq}\n'.encode())
 
 assert found == set(meta.index), 'Not all bins were found in the barrnap output!'
